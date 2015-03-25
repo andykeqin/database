2a8f9f31b68995e4ccf6e2e75041389ebf06a0d8
andykeqin <keqin.feng.12@ucl.ac.uk>
2015-03-25 19:22:15 +0000

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
     <div class="container-fluid">
         <hr />
         <footer>
-            <p>Peer Assessment &copy; 2015</p>
+            <p>COMP3013 Peer Assessment &copy; 2015</p>
         </footer>
     </div>
 </body>
