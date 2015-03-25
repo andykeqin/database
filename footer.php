747f0ce5ef548f469354c5c649f88520442a8690
CaitlinJ <icesymeng@gmail.com>
2015-03-25 19:20:52 +0000

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
     <div class="container-fluid">
         <hr />
         <footer>
-            <p>&copy; 2015</p>
+            <p>Peer Assessment &copy; 2015</p>
         </footer>
     </div>
 </body>
