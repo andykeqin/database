6e1dfd97a310fa1fd9e5a62a4b811bd42837c087
CaitlinJ <icesymeng@gmail.com>
2015-03-25 22:06:59 +0000

    Update signin.php

--- signin.php
+++ signin.php
@@ -92,10 +92,10 @@
             <span>
                 <div class="container">
                     <form class="form-signin" action="" method="post">
-                        <h2 class="form-signin-heading">Please sign in</h2>
+                        <h2 class="form-signin-heading">Peer Assessment</h2>
                         <input type="text" name="email" class="input-block-level" placeholder="Email address" />
                         <input type="password" name="password" class="input-block-level" placeholder="Password" />
-                        <button class="btn btn-large btn-primary" type="submit">Sign in</button>
+                        <button class="btn btn-large btn-primary" type="submit">Log in</button>
                     </form>
                 </div>
             </span>
